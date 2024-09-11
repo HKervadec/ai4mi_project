c61ffe4df4dfccc9193303e217a6a224695b38fb
danilotpnta <danilotpnta@gmail.com>
2024-09-11 22:09:42 +0200

    wip: added HD and IoU metric for evaluation

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,6 +1,8 @@
 import torch
-from torch import Tensor, einsum
+import numpy as np
+from medpy import metric
 from functools import partial
+from torch import Tensor, einsum
 from utils.tensor_utils import one_hot, sset
 
 def meta_dice(
@@ -46,3 +48,28 @@ def union(a: Tensor, b: Tensor) -> Tensor:
     assert sset(res, [0, 1])
 
     return res
+
+
+def iou_coef(label: Tensor, pred: Tensor, smooth: float = 1e-8) -> Tensor:
+    assert label.shape == pred.shape
+
+    inter_size: Tensor = intersection(label, pred).sum().float()
+    union_size: Tensor = union(label, pred).sum().float()
+
+    return (inter_size + smooth) / (union_size + smooth)
+
+
+def hausdorff_distance(pred: Tensor, label: Tensor) -> Tensor:
+    # implementation based on
+    # https://github.com/nazib/MIDL_code/blob/hpc/evaluate.py
+    assert pred.shape == label.shape
+    assert sset(pred, [0, 1]) and sset(label, [0, 1])
+
+    pred_np = pred.cpu().numpy()
+    label_np = label.cpu().numpy()
+
+    if np.sum(label_np) == 0 or np.sum(pred_np) == 0:
+        return torch.tensor(float('nan'))
+
+    return torch.tensor(metric.hd(pred_np, label_np))
+
