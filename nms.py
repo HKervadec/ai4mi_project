6040da9ba3da77c6cfb053ce4a8d28ab6c5e00e2
Marga Snellius <marga.don@student.uva.nl>
2024-09-25 14:02:12 +0200

    final version

--- nms.py
+++ nms.py
@@ -15,11 +15,12 @@ import os
 def main(args):
     # Select only files ending in .nii.gz
     target_volumes = os.listdir(args.src)
-    target_volumes = [x for x in target_volumes if '.nii.gz' in x ]
+    target_volumes = [x for x in target_volumes if '.nii.gz' in x]
     print('Found volumes:', target_volumes)
 
     for vol_path in tqdm_(target_volumes):
         result = nms(f'{args.src}/{vol_path}')
+        nib.save(result, f'{args.dest}/{vol_path}')
 
 def nms(path):
     vol_nib = nib.load(path)
@@ -27,8 +28,9 @@ def nms(path):
 
     # Split and label per class
     classes = [0,1,2,3,4]
+    vol = vol / 63
     split = [np.where(vol == x, x, 0) for x in classes]
-    split_labeled = [label(x) for x in split]
+    split_labeled = [label(x, connectivity=2) for x in split]
 
     # Keep only the largest area per class
     split_nms = [np.zeros(vol.shape)] * 5
@@ -37,18 +39,19 @@ def nms(path):
 
         if len(counts) > 1:
             indices, counts = indices[1:], counts[1:]
-        most_occuring = indices[np.argmax(counts)]
-        selected = np.where(l == most_occuring, 1, 0)
+            most_occuring = indices[np.argmax(counts)]
+            selected = np.where(l == most_occuring, 1, 0)
 
-        # Put the nms processed volume in its right spot
-        # and make sure the values match the class index
-        split_nms[i] = selected * i
-
-    result = np.vstack(split_nms)
-    print(result.shape)
-    print(np.hstack(split_nms.shape), np.stack(split_nms.shape))
-    quit()
-    # split_nibs = [nib.nifti1.Nifti1Image(x, affine=vol_nib.affine, header=vol_nib.header) for x in gt_split_labeled]
+            # Put the nms processed volume in its right spot
+            # and make sure the values match the class index
+            split_nms[i] = selected * i * 63
+    
+    # Combine classes again and correct type
+    result = np.sum(split_nms, axis=0)
+    result = np.astype(result, np.uint8)
+    assert result.shape == vol.shape
+    result = nib.nifti1.Nifti1Image(result, affine=vol_nib.affine, header=vol_nib.header)
+    return result
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
