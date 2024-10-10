f9b67eecb332aec4d7b86a57d7faf8372014e91f
raoulritter <raoulflorisritter@gmail.com>
2024-10-10 08:25:38 +0200

    Add pepijns changes to his branch

---
+++ VMUnet.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3.10
+
+# MIT License
+
+# Copyright (c) 2024 Hoel Kervadec, Jose Dolz
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+"""
+This file contains our code implementation for VM-Unet.
+The code is based on https://github.com/JCruan519/VM-UNet
+the format of the code is based on ENet.py.
+
+
+
+Note, the following requirements needs to be installed to run the model:
+
+pip install torch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install packaging
+pip install timm==0.4.12
+pip install pytest chardet yacs termcolor
+pip install submitit tensorboardX
+pip install triton==2.0.0
+pip install causal_conv1d==1.0.0  # causal_conv1d-1.0.0+cu118torch1.13cxx11abiFALSE-cp38-cp38-linux_x86_64.whl
+pip install mamba_ssm==1.0.1  # mmamba_ssm-1.0.1+cu118torch1.13cxx11abiFALSE-cp38-cp38-linux_x86_64.whl
+pip install scikit-learn matplotlib thop h5py SimpleITK scikit-image medpy yacs
+
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+from vmamba import VSSM
+
+class VMUNet(nn.Module):
+    """
+    Code obtained from:
+    https://github.com/JCruan519/VM-UNet/blob/main/models/vmunet/vmunet.py
+
+    Set input num_channels to 5
+    """
+    def __init__(self, 
+                 input_channels=1, 
+                 num_classes=5,
+                 depths=[2, 2, 9, 2], 
+                 depths_decoder=[2, 9, 2, 2],
+                 drop_path_rate=0.2,
+                 load_ckpt_path='./pre_trained_weights/vmamba_small_e238_ema.pth',
+                ):
+        super().__init__()
+
+        self.load_ckpt_path = load_ckpt_path
+        self.num_classes = num_classes
+
+        self.vmunet = VSSM(in_chans=input_channels,
+                           num_classes=num_classes,
+                           depths=depths,
+                           depths_decoder=depths_decoder,
+                           drop_path_rate=drop_path_rate,
+                        )
+    
+    def forward(self, x):
+        if x.size()[1] == 1:
+            x = x.repeat(1,3,1,1)
+        logits = self.vmunet(x)
+        if self.num_classes == 1: return torch.sigmoid(logits)
+        else: return logits
+    
+    def load_from(self):
+        if self.load_ckpt_path is not None:
+            model_dict = self.vmunet.state_dict()
+            modelCheckpoint = torch.load(self.load_ckpt_path)
+            pretrained_dict = modelCheckpoint['model']
+            
+            new_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict.keys()}
+            model_dict.update(new_dict)
+            
+            print('Total model_dict: {}, Total pretrained_dict: {}, update: {}'.format(len(model_dict), len(pretrained_dict), len(new_dict)))
+            self.vmunet.load_state_dict(model_dict)
+
+            not_loaded_keys = [k for k in pretrained_dict.keys() if k not in new_dict.keys()]
+            print('Not loaded keys:', not_loaded_keys)
+            print("encoder loaded finished!")
+
+            model_dict = self.vmunet.state_dict()
+            modelCheckpoint = torch.load(self.load_ckpt_path)
+            pretrained_odict = modelCheckpoint['model']
+            pretrained_dict = {}
+            for k, v in pretrained_odict.items():
+                if 'layers.0' in k: 
+                    new_k = k.replace('layers.0', 'layers_up.3')
+                    pretrained_dict[new_k] = v
+                elif 'layers.1' in k: 
+                    new_k = k.replace('layers.1', 'layers_up.2')
+                    pretrained_dict[new_k] = v
+                elif 'layers.2' in k: 
+                    new_k = k.replace('layers.2', 'layers_up.1')
+                    pretrained_dict[new_k] = v
+                elif 'layers.3' in k: 
+                    new_k = k.replace('layers.3', 'layers_up.0')
+                    pretrained_dict[new_k] = v
+            # 过滤操作
+            new_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict.keys()}
+            model_dict.update(new_dict)
+            # 打印出来，更新了多少的参数
+            print('Total model_dict: {}, Total pretrained_dict: {}, update: {}'.format(len(model_dict), len(pretrained_dict), len(new_dict)))
+            self.vmunet.load_state_dict(model_dict)
+            
+            # 找到没有加载的键(keys)
+            not_loaded_keys = [k for k in pretrained_dict.keys() if k not in new_dict.keys()]
+            print('Not loaded keys:', not_loaded_keys)
+            print("decoder loaded finished!")
+
+
+    def enable_finetuning(self):
+        # Disable gradient updating for all parameters 
+        for param in self.vmunet.parameters():
+            param.requires_grad = False
+
+        # Enable gradient updates for last conv layer
+        for param in self.vmunet.final_conv.parameters():
+            param.requires_grad = True
+
+        # Enable gradient updates for last upsampling layer
+        for param in self.vmunet.final_up.parameters():
+            param.requires_grad = True
