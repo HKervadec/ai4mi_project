1821b312d4543452632e11f18c9801820f8af8db
BatuHelva <46646113+BatuHelva@users.noreply.github.com>
2024-10-12 01:42:49 +0200

    debugging slow training v2

--- main.py
+++ main.py
@@ -230,11 +230,11 @@ def runTraining(args):
                     # IoU (Jaccard Index)
                     log_iou[e, j:j + B, :] = iou_coef(pred_seg, gt)
 
-                    # # Hausdorff Distance
-                    # log_hausdorff[e, j:j + B, :] = hausdorff_coef(pred_seg, gt).transpose(0, 1)
+                    # Hausdorff Distance
+                    log_hausdorff[e, j:j + B, :] = hausdorff_coef(pred_seg, gt).transpose(0, 1)
 
                     # ASSD (Average Symmetric Surface Distance)
-                    log_assd[e, j:j + B, :] = assd_coef(pred_seg, gt).transpose(0, 1)
+                    # log_assd[e, j:j + B, :] = assd_coef(pred_seg, gt).transpose(0, 1)
 
                     # Volumetric Similarity
                     log_volsim[e, j:j + B, :] = vol_sim_coef(pred_seg, gt)
