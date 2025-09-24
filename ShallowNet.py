e78c105147e8e45eca06d72739464f43196b5be9
Caroline Magg <c.magg@amsterdamumc.nl>
2025-09-24 17:25:32 +0200

    fixes lambda pickling. fixes unexpected kwargs to shallowNET

--- ShallowNet.py
+++ ShallowNet.py
@@ -34,7 +34,7 @@ def convBatch(nin, nout, kernel_size=3, stride=1, padding=1, bias=False, layer=n
 
 
 class shallowCNN(nn.Module):
-    def __init__(self, nin, nout, nG=4):
+    def __init__(self, nin, nout, nG=4, **kwargs): # **kwargs discards unnecessary keywords arguments (kernels, factor)
         super(shallowCNN, self).__init__()
         self.conv0 = convBatch(nin, nG * 4)
         self.conv1 = convBatch(nG * 4, nG * 4)
--- main.py
+++ main.py
@@ -37,6 +37,8 @@ from torch import nn, Tensor
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
+from functools import partial 
+
 from dataset import SliceDataset
 from ShallowNet import shallowCNN
 from ENet import ENet
@@ -57,6 +59,23 @@ datasets_params["TOY2"] = {'K': 2, 'net': shallowCNN, 'B': 2, 'kernels': 8, 'fac
 datasets_params["SEGTHOR"] = {'K': 5, 'net': ENet, 'B': 8, 'kernels': 8, 'factor': 2}
 datasets_params["SEGTHOR_CLEAN"] = {'K': 5, 'net': ENet, 'B': 8, 'kernels': 8, 'factor': 2}
 
+def img_transform(img):
+        img = img.convert('L')
+        img = np.array(img)[np.newaxis, ...]
+        img = img / 255  # max <= 1
+        img = torch.tensor(img, dtype=torch.float32)
+        return img
+
+def gt_transform(K, img):
+        img = np.array(img)[...]
+        # The idea is that the classes are mapped to {0, 255} for binary cases
+        # {0, 85, 170, 255} for 4 classes
+        # {0, 51, 102, 153, 204, 255} for 6 classes
+        # Very sketchy but that works here and that simplifies visualization
+        img = img / (255 / (K - 1)) if K != 5 else img / 63  # max <= 1
+        img = torch.tensor(img, dtype=torch.int64)[None, ...]  # Add one dimension to simulate batch
+        img = class2one_hot(img, K=K)
+        return img[0]
 
 def setup(args) -> tuple[nn.Module, Any, Any, DataLoader, DataLoader, int]:
     # Networks and scheduler
@@ -78,29 +97,12 @@ def setup(args) -> tuple[nn.Module, Any, Any, DataLoader, DataLoader, int]:
     B: int = datasets_params[args.dataset]['B']
     root_dir = Path("data") / args.dataset
 
-    img_transform = transforms.Compose([
-        lambda img: img.convert('L'),
-        lambda img: np.array(img)[np.newaxis, ...],
-        lambda nd: nd / 255,  # max <= 1
-        lambda nd: torch.tensor(nd, dtype=torch.float32)
-    ])
 
-    gt_transform = transforms.Compose([
-        lambda img: np.array(img)[...],
-        # The idea is that the classes are mapped to {0, 255} for binary cases
-        # {0, 85, 170, 255} for 4 classes
-        # {0, 51, 102, 153, 204, 255} for 6 classes
-        # Very sketchy but that works here and that simplifies visualization
-        lambda nd: nd / (255 / (K - 1)) if K != 5 else nd / 63,  # max <= 1
-        lambda nd: torch.tensor(nd, dtype=torch.int64)[None, ...],  # Add one dimension to simulate batch
-        lambda t: class2one_hot(t, K=K),
-        itemgetter(0)
-    ])
 
     train_set = SliceDataset('train',
                              root_dir,
                              img_transform=img_transform,
-                             gt_transform=gt_transform,
+                             gt_transform= partial(gt_transform, K),
                              debug=args.debug)
     train_loader = DataLoader(train_set,
                               batch_size=B,
@@ -110,7 +112,7 @@ def setup(args) -> tuple[nn.Module, Any, Any, DataLoader, DataLoader, int]:
     val_set = SliceDataset('val',
                            root_dir,
                            img_transform=img_transform,
-                           gt_transform=gt_transform,
+                           gt_transform=partial(gt_transform, K),
                            debug=args.debug)
     val_loader = DataLoader(val_set,
                             batch_size=B,
