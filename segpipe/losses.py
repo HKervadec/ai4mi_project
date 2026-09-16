b843f8f5f5c9b9c1ae85af8a814f3ca42563e5b5
David Borensztajn <73830341+DavidBorensztajnHub@users.noreply.github.com>
2026-09-16 23:13:23 +0200

    add example augment yaml

--- segpipe/losses.py
+++ segpipe/losses.py
@@ -7,36 +7,16 @@ LOSSES: dict = {
     "ce": CrossEntropy,
 }
 
-# name -> class with __init__(**params) and __call__(probs, onehot_target) -> scalar
+# Not used by build_loss yet
 REGULARIZERS: dict = {}
 
 
-class WithRegularizers():
-    def __init__(self, loss, regularizers: list[tuple[float, object]]):
-        self.loss = loss
-        self.regularizers = regularizers
-
-    def __call__(self, pred_softmax, weak_target):
-        total = self.loss(pred_softmax, weak_target)
-        for weight, regularizer in self.regularizers:
-            total = total + weight * regularizer(pred_softmax, weak_target)
-        return total
-
-
 def build_loss(cfg, K: int):
+    if cfg.get("regularizers"):
+        raise NotImplementedError("regularizers are not added to the loss yet")
     params = dict(cfg.loss)
     name = params.pop("name")
     classes = params.pop("classes", "all")
     if name not in LOSSES:
         raise KeyError(f"unknown loss '{name}'. Known: {sorted(LOSSES)}")
-    loss = LOSSES[name](idk=list(range(K)) if classes == "all" else list(classes), **params)
-
-    regularizers = []
-    for item in cfg.get("regularizers") or []:
-        reg_params = dict(item)
-        reg_name = reg_params.pop("name")
-        weight = reg_params.pop("weight", 1.0)
-        if reg_name not in REGULARIZERS:
-            raise KeyError(f"unknown regularizer '{reg_name}'. Known: {sorted(REGULARIZERS)}")
-        regularizers.append((weight, REGULARIZERS[reg_name](**reg_params)))
-    return WithRegularizers(loss, regularizers) if regularizers else loss
+    return LOSSES[name](idk=list(range(K)) if classes == "all" else list(classes), **params)
